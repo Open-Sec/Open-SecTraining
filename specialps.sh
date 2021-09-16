a82b0875c1eebad9f47432bbce3c40d205b2b542
Open Sec <wcuestas@gmail.com>
2021-09-15 22:13:31 -0500

    Update specialps.sh

--- specialps.sh
+++ specialps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Execute : bash specialps.sh file-targets-ports.lst
+#Execute : bash specialps.sh file-targets-ports.lst nmap-reports-name
 #File format:
 #ip_address1 port1,port2,port3,...
 #ip_address2 port4,port5,port6....
 
-for line in `cat targets.lst`
+for line in `cat $1`
 	do 
 		possibletarget=`echo $line | grep -o "\." | wc -l`
 		if [ $possibletarget -eq 3 ]
@@ -15,9 +15,8 @@ for line in `cat targets.lst`
 		fi
 		if [ ! -z "$target" ] && [ ! -z "$ports" ]
 		then
-			( nmap -n -v -Pn -sT --open --disable-arp-ping -p `echo $ports` `echo $target`)
+			( nmap -n -v -Pn -sT --open --disable-arp-ping -sV -p `echo $ports` `echo $target` -oA `echo $2 --append-output`)
 			target=""
 			ports=""
 		fi
 	done
-
