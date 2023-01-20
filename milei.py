f40a3c6c68b5e0941f03ce9e8ef351596c32c371
Open Sec <wcuestas@gmail.com>
2023-01-20 02:36:25 -0500

    Update milei.py

--- milei.py
+++ milei.py
@@ -17,7 +17,7 @@ read_response = client.secrets.kv.read_secret_version(path='my-secret-password')
 
 password = read_response['data']['data']['password']
 
-
+# As a way to check if the secret was correctly stored. In real world, It'll be so silly to hardcode the secret you want to hide.
 if password != 'MileiPresidente':
     sys.exit('unexpected password')
 
