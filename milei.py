883be3f6dc7833f8b0005c2a12f7c9f0ba5a47d3
Open Sec <wcuestas@gmail.com>
2023-12-13 18:38:14 -0500

    Update milei.py

--- milei.py
+++ milei.py
@@ -1,24 +1,28 @@
 import hvac
+import sys
 
 client = hvac.Client(
     url='http://127.0.0.1:8200',
     token='eko2022',
-)
+)   
 
-create_response = client.secrets.kv.v2.create_or_update_secret(
-    path='my-secret-password',
-    secret=dict(password='MileiPresidente'),
-)
+if sys.argv[1] == 'G':
+    pwd_input=input("Please set your password:")
+    create_response = client.secrets.kv.v2.create_or_update_secret(
+        path='my-secret-password',
+        secret=dict(password=pwd_input),
+    )   
 
-print('Secreto registrado!.')
+    print('Secret recorded!.')
 
+if sys.argv[1] == 'A':
+    pwd_validation=input("Enter your password AGAIN:")
 
-read_response = client.secrets.kv.read_secret_version(path='my-secret-password')
+    read_response = client.secrets.kv.read_secret_version(path='my-secret-password',raise_on_deleted_version=True)
 
-password = read_response['data']['data']['password']
+    password = read_response['data']['data']['password']
 
-# As a way to check if the secret was correctly stored. In real world, It'll be so silly to hardcode the secret you want to hide.
-if password != 'MileiPresidente':
-    sys.exit('unexpected password')
-
-print('MILEI PRESIDENTE CARAJO!')
+    if password != pwd_validation:
+        sys.exit('unexpected password')
+    else:
+        print('MILEI PRESIDENTE CARAJO!')
